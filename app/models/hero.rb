9783536fa770ee516a5ed47cde0e6de1bd6606da
Meredith Reece <mardebugger@gmail.com>
2018-01-26 00:01:23 +0000

    automatically backed up by learn

--- app/models/hero.rb
+++ app/models/hero.rb
@@ -1,11 +1,16 @@
 class Hero
   attr_reader :name, :power, :bio
-
+  @@all_heroes = []
+  
   def initialize(details)
     @name = details[:name]
     @power = details[:power]
     @bio = details[:bio]
 
     @@all_heroes << self
+  end
+
+  def self.all
+    @@all_heroes
   end
 end
